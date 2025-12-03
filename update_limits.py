0b4794a688b2c4168412bce03cdc230035158f09
Nathan-Houel <houel.nathan.18@gmail.com>
2025-12-03 01:30:39 +0100

    Change little things on it

--- update_limits.py
+++ update_limits.py
@@ -1,23 +1,23 @@
 import sqlite3
 from datetime import datetime
 
-# Opening the database
-conn = sqlite3.connect('avionics.db')
-cur = conn.cursor()
 
-equipment_id = 10
-new_limit = 126
+def update_spec(equip_id, new_val):
+    
+    # Opening the database
+    conn = sqlite3.connect('avionics.db')
+    cur = conn.cursor()
 
-# Retrieve old value
-old_value = cur.execute("SELECT max_value FROM Limits WHERE ID = ?", (equipment_id,)).fetchone()[0]
+    # Retrieve old value
+    old_value = cur.execute("SELECT max_value FROM Limits WHERE ID = ?", (equip_id,)).fetchone()[0]
 
-# Modifying the value
-cur.execute("UPDATE Limits SET max_value = ? WHERE ID = ?", (new_limit, equipment_id))
+    # Modifying the value
+    cur.execute("UPDATE Limits SET max_value = ? WHERE ID = ?", (new_val, equip_id))
 
-# Adding the data in the History table
-cur.execute("INSERT OR IGNORE INTO History \
-            (equipment_id, old_value, new_value, change_date) VALUES (?, ?, ?, ?)", \
-            (equipment_id, old_value, new_limit, datetime.now().isoformat()))
+    # Adding the data in the History table
+    cur.execute("INSERT OR IGNORE INTO History \
+                (equipment_id, old_value, new_value, change_date) VALUES (?, ?, ?, ?)", \
+                (equip_id, old_value, new_val, datetime.now().isoformat()))
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
